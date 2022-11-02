47a5050b1df42a5c45a016704f149632a9fc0689
shmudivel <82043832+shmudivel@users.noreply.github.com>
2022-11-02 18:10:02 -0400

    hours fixed condition

--- my-react-app/src/components/ListOfHours.jsx
+++ my-react-app/src/components/ListOfHours.jsx
@@ -11,42 +11,66 @@ const ListOfHours = (props) => {
 
       // setClockState(dateAndGMT.getTimezoneOffset())
       setTimeGmt(date.getUTCHours());
-    }, 1000);
+    }, 2000);
   }, []);
+
+//   const gmtPlusOffsetFromSelect = timeGmt + props.num;
+
+function reduceGmt(hour) {
+    // const totalHours = ((timeGmt/24 + (props.num/24)) % 1) * 24 + hour
+    const totalHours = timeGmt + props.num + hour
+    if (totalHours < 24) {
+        return totalHours;
+    } else if (totalHours < 36) {
+        return totalHours - 24;
+    } else if ( totalHours < 48){
+        return totalHours - 24;
+    } else {
+        return totalHours - 48;
+    } 
+        // return cosnt totalHours = ((timeGmt/24 + (props.num/24)) % 1) * 24 + hour
+}
+
+// const testMODexcel = timeGmt + (props.num / 24)
+// const testMODexcel = 120 + (4 / 24)
+// console.log(testMODexcel);
+
+// const testExcelAlbina = ((timeGmt/24 + (props.num/24)) % 1) * 24
+  
 
   return (
     <div>
       <div>
-        {/* <div>GMT: {timeGmt}</div> */}
+        <div>GMT: {timeGmt}</div>
         <h1 className="">{props.title}</h1>
-        {/* <h2>{props.num}</h2> */}
+        <h2>{props.num}</h2>
       </div>
 
       <ul>
-        <li>{timeGmt + props.num}</li>
-        <li>{timeGmt + props.num + 1}</li>
-        <li>{timeGmt + props.num + 2}</li>
-        <li>{timeGmt + props.num + 3}</li>
-        <li>{timeGmt + props.num + 4}</li>
-        <li>{timeGmt + props.num + 5}</li>
-        <li>{timeGmt + props.num + 6}</li>
-        <li>{timeGmt + props.num + 7}</li>
-        <li>{timeGmt + props.num + 8}</li>
-        <li>{timeGmt + props.num + 9}</li>
-        <li>{timeGmt + props.num + 10}</li>
-        <li>{timeGmt + props.num + 11}</li>
-        <li>{timeGmt + props.num + 12}</li>
-        <li>{timeGmt + props.num + 13}</li>
-        <li>{timeGmt + props.num + 14}</li>
-        <li>{timeGmt + props.num + 15}</li>
-        <li>{timeGmt + props.num + 16}</li>
-        <li>{timeGmt + props.num + 17}</li>
-        <li>{timeGmt + props.num + 18}</li>
-        <li>{timeGmt + props.num + 19}</li>
-        <li>{timeGmt + props.num + 20}</li>
-        <li>{timeGmt + props.num + 21}</li>
-        <li>{timeGmt + props.num + 22}</li>
-        <li>{timeGmt + props.num + 23}</li>
+        <li>{reduceGmt(0)}</li>
+        <li>{reduceGmt(1)}</li>
+        <li>{reduceGmt(2)}</li>
+        <li>{reduceGmt(3)}</li>
+        <li>{reduceGmt(4)}</li>
+        <li>{reduceGmt(5)}</li>
+        <li>{reduceGmt(6)}</li>
+        <li>{reduceGmt(7)}</li>
+        <li>{reduceGmt(8)}</li>
+        <li>{reduceGmt(9)}</li>
+        <li>{reduceGmt(10)}</li>
+        <li>{reduceGmt(11)}</li>
+        <li>{reduceGmt(12)}</li>
+        <li>{reduceGmt(13)}</li>
+        <li>{reduceGmt(14)}</li>
+        <li>{reduceGmt(15)}</li>
+        <li>{reduceGmt(16)}</li>
+        <li>{reduceGmt(17)}</li>
+        <li>{reduceGmt(18)}</li>
+        <li>{reduceGmt(19)}</li>
+        <li>{reduceGmt(20)}</li>
+        <li>{reduceGmt(21)}</li>
+        <li>{reduceGmt(22)}</li>
+        <li>{reduceGmt(23)}</li>
       </ul>
     </div>
   );
