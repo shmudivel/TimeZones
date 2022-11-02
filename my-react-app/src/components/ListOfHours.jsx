e9a0dd90b754e43fedfd63627171f85ff1443fa5
shmudivel <82043832+shmudivel@users.noreply.github.com>
2022-11-02 10:07:12 -0400

    select TZ by hourly

--- my-react-app/src/components/ListOfHours.jsx
+++ my-react-app/src/components/ListOfHours.jsx
@@ -1,59 +1,53 @@
-
 import React, { useEffect, useState } from "react";
 
 const ListOfHours = (props) => {
-    const [clockState, setClockState] = useState();
+  const [timeGmt, setTimeGmt] = useState();
 
-    useEffect(() => {
-        setInterval(() => {
-            const date = new Date();
+  useEffect(() => {
+    setInterval(() => {
+      const date = new Date();
 
-            // const dateAndGMT = date.toGMTString()
+      // const dateAndGMT = date.toGMTString()
 
-            // setClockState(dateAndGMT.getTimezoneOffset())
-            setClockState(date.getTimezoneOffset())
-        }, 1000)
-    }, [])
+      // setClockState(dateAndGMT.getTimezoneOffset())
+      setTimeGmt(date.getUTCHours());
+    }, 1000);
+  }, []);
 
-    return (
+  return (
     <div>
       <div>
-      <div>GMT: {clockState}</div>
+        {/* <div>GMT: {timeGmt}</div> */}
         <h1 className="">{props.title}</h1>
-        <h2>{props.num}</h2>
-
-        
-
+        {/* <h2>{props.num}</h2> */}
       </div>
 
-
-      <ul >
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-        <li>6</li>
-        <li>7</li>
-        <li>8</li>
-        <li>9</li>
-        <li>10</li>
-        <li>11</li>
-        <li>12</li>
-        <li>13</li>
-        <li>14</li>
-        <li>15</li>
-        <li>16</li>
-        <li>17</li>
-        <li>18</li>
-        <li>19</li>
-        <li>20</li>
-        <li>21</li>
-        <li>22</li>
-        <li>23</li>
-        <li>24</li>
+      <ul>
+        <li>{timeGmt + props.num}</li>
+        <li>{timeGmt + props.num + 1}</li>
+        <li>{timeGmt + props.num + 2}</li>
+        <li>{timeGmt + props.num + 3}</li>
+        <li>{timeGmt + props.num + 4}</li>
+        <li>{timeGmt + props.num + 5}</li>
+        <li>{timeGmt + props.num + 6}</li>
+        <li>{timeGmt + props.num + 7}</li>
+        <li>{timeGmt + props.num + 8}</li>
+        <li>{timeGmt + props.num + 9}</li>
+        <li>{timeGmt + props.num + 10}</li>
+        <li>{timeGmt + props.num + 11}</li>
+        <li>{timeGmt + props.num + 12}</li>
+        <li>{timeGmt + props.num + 13}</li>
+        <li>{timeGmt + props.num + 14}</li>
+        <li>{timeGmt + props.num + 15}</li>
+        <li>{timeGmt + props.num + 16}</li>
+        <li>{timeGmt + props.num + 17}</li>
+        <li>{timeGmt + props.num + 18}</li>
+        <li>{timeGmt + props.num + 19}</li>
+        <li>{timeGmt + props.num + 20}</li>
+        <li>{timeGmt + props.num + 21}</li>
+        <li>{timeGmt + props.num + 22}</li>
+        <li>{timeGmt + props.num + 23}</li>
       </ul>
-
     </div>
   );
 };
